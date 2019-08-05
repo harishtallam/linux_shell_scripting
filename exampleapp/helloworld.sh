9c0e0e53e0acc686a21b24ec254b6192286cb38e
Harish Tallam <harishtallam@gmail.com>
2019-08-05 12:04:41 +0530

    Another commit

---
+++ exampleapp/helloworld.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo Hello World
+echo "Hello World"
+
+echo $BASH # Usually default system variables are maintained --
+echo $HOME # using capital letter, hence we have used it
+echo $PWD
+
+name = Harish
+echo "The name is" $name
+
+value = 100
+echo $value
+
+
+value = 10
+echo $value
