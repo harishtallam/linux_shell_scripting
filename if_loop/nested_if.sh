13b4840ee9a3ced12c260c45979203b05ebb3066
harishtallam <harishtallam@gmail.com>
2019-08-30 05:53:50 +0530

    nested if condition

---
+++ if_loop/nested_if.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo -e "Enter the file name: \c"
+read filename
+
+if [ -f $filename ]
+then
+    if [ -w $filename ]
+    then
+        echo "Type some text. Press CTRL+C or CTRL+D to quit"
+        cat >> $filename
+    else
+        echo "The file do not have write permissions"
+    fi
+else
+    echo -e "$filename not found"
+fi
