cd043db0296f322bb3c7568f3e40939b7432de8e
harishtallam <harishtallam@gmail.com>
2019-08-30 08:08:29 +0530

    arithmetic operators

---
+++ arithmetic_operators/arithmetic_op1.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+num1=30
+num2=2
+
+echo $((num1 + num2))
+echo $((num1 - num2))
+echo $((num1 * num2))
+echo $((num1 / num2))
+echo $((num1 % num2))
+
+echo ""
+
+num1=31
+num2=2
+
+echo $(expr $num1 + $num2)
+echo $(expr $num1 - $num2)
+echo $(expr $num1 \* $num2) #If we use expr for multiplication, * to be escaped using escape character
+echo $(expr $num1 / $num2)
+echo $(expr $num1 % $num2)
---
+++ arithmetic_operators/arithmetic_op2.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+num1=30
+num2=2
+
+echo "20.5+5" | bc
+echo "20.5-5" | bc
+echo "20.5*5" | bc
+echo "scale=2;20.5/5" | bc #scale helps in defining the number of decimals required for devision operation
+echo "20.5%5" | bc
+
+echo ""
+
+echo "$num1+$num2" | bc
+echo "$num1-$num2" | bc
+echo "$num1*$num2" | bc
+echo "scale=2;$num1/$num2" | bc #scale helps in defining the number of decimals required for devision operation
+echo "$num1%$num2" | bc
+
+echo ""
+
+num=25
+
+echo "scale=2;sqrt($num1)" | bc -l # As "sqrt" is math operator, "-l" is required to call math operators
+echo "scale=2;3^3" | bc -l # ^ indicates power; 3 to the power of 3
